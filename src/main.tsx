807bde41b2f80479d0e0cd5ed501f0a9da47adc1
justamix <fmmf20@yandex.ru>
2024-11-19 00:18:19 +0300

    fedya2

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,29 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'; // Импортируем Provider
+import { store } from './store'; // Импортируем ваш store
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter future={{
-    v7_startTransition: true,
-    v7_relativeSplatPath: true,
-  }} basename="/FRONTEND">
-        <App />
-  </BrowserRouter>
-
-)
+  <Provider store={store}> {/* Оборачиваем Provider */}
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+      basename="/FRONTEND"
+    >
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
       .register('/FRONTEND/serviceWorker.js')
       .then((res) => console.log('Service Worker registered', res))
       .catch((err) => console.log('Service Worker not registered', err));
-    })
-  }
+  });
+}
