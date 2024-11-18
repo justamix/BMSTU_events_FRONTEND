8be8cb8a80b0788af369250dd396aa633c2f0813
justamix <fmmf20@yandex.ru>
2024-11-18 21:40:24 +0300

    try

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter basename="my-app">
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter future={{
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+  }} basename="/FRONTEND">
         <App />
-    </BrowserRouter>
+  </BrowserRouter>
+
 )
