b37d7bc655d36051f56cf91244041ac1ed4869df
justamix <fmmf20@yandex.ru>
2024-12-22 17:48:42 +0300

    aaasdsd

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             "/api": {
-                target: "http://localhost:8000",
+                target: "http://192.168.68.107:8000",
                 changeOrigin: true,
                 secure: false,
             }
